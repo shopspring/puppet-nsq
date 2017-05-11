6823a90951f20dfbdc635d81e5942c688e494e05
Justin Lintz <justin@shopspring.com>
2017-05-11 10:51:15 -0400

    Documentation and parameter checking

--- manifests/nsqlookupd.pp
+++ manifests/nsqlookupd.pp
@@ -1,24 +1,43 @@
 # Class: nsq::nsqlookupd
 # ===========================
 #
-# Full description of class nsq here.
+# Responsible for configuration and management of nsqlookupd
 #
 # Parameters
 # ----------
 #
-# * `sample parameter`
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# * `manage_service`
+#   Installs a systemd unit file and registers it as a service
+#
+# * `ensure_running`
+#   Ensure nsqd service is running
+#
+# * `verbose_logging`
+#   Toggle verbose logging
+#
+# * `tcp_address`
+#   The IP and port nsqd will bind to for TCP connections
+#   e.g. 0.0.0.0:4160
+#
+# * `http_address`
+#   The IP and port nsqd will bind to for TCP connections
+#   e.g. 0.0.0.0:4161
+#
 #
 class nsq::nsqlookupd(
   Boolean $manage_service  = $::nsq::params::manage_service,
   Boolean $ensure_running  = $::nsq::params::ensure_running,
+  Boolean $verbose_logging = false,
   String $tcp_address      = '0.0.0.0:4160',
   String $http_address     = '0.0.0.0:4161',
-  Boolean $verbose_logging = false,
 ){
 # configure and setup service
   include nsq::nsqlookupd::config
+
+  if $ensure_running {
+    assert_type(Boolean, $manage_service)
+    if ! $manage_service { fail ('$manage_service must be True if using $ensure_running') }
+  }
 
   if $manage_service {
     # put upstart file in place
