d22bd78aa54f7f273b0dd462da90a87940e8999f
Justin Lintz <justin@shopspring.com>
2017-05-11 10:17:20 -0400

    Documentation and error check

--- manifests/nsqadmin.pp
+++ manifests/nsqadmin.pp
@@ -1,14 +1,22 @@
 # Class: nsq::nsqadmin
 # ===========================
 #
-# Full description of class nsq here.
+# Responsible for configuration and management of nsqadmin
 #
 # Parameters
 # ----------
 #
-# * `sample parameter`
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# * `manage_service`
+#   Installs a systemd unit file and registers it as a service
+#
+# * `ensure_running`
+#   Ensure nsqadmin service is running
+#
+# * `http_address`
+#   The IP and port nsqadmin will bind to.  e.g. 0.0.0.0:4171
+#
+# * `nsqlookupd_addresses`
+#   Array of nsqlookupd addresses to connect to
 #
 class nsq::nsqadmin(
   Boolean $manage_service     = $::nsq::params::manage_service,
@@ -17,6 +25,11 @@ class nsq::nsqadmin(
   Array $nsqlookupd_addresses = [ '127.0.0.1:4161' ],
 ){
   include nsq::nsqadmin::config
+
+  if $ensure_running {
+    assert_type(Boolean, $manage_service)
+    if ! $manage_service { fail ('$manage_service must be True if using $ensure_running') }
+  }
 
   if $manage_service {
     # put upstart file in place
