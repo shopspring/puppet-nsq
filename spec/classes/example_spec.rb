f5a9b00b114f257fbea7a0f64e8fdb7b0694d341
Justin Lintz <justin@shopspring.com>
2017-05-11 15:27:00 -0400

    Testing cleanup

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -12,27 +12,12 @@ describe 'nsq' do
           it { is_expected.to compile.with_all_deps }
 
           it { is_expected.to contain_class('nsq::params') }
-          it { is_expected.to contain_class('nsq::install').that_comes_before('nsq::config') }
-          it { is_expected.to contain_class('nsq::config') }
+          it { is_expected.to contain_class('nsq::install').that_comes_before('nsq::service') }
           it { is_expected.to contain_class('nsq::service').that_subscribes_to('nsq::config') }
 
           it { is_expected.to contain_service('nsq') }
-          it { is_expected.to contain_package('nsq').with_ensure('present') }
         end
       end
-    end
-  end
-
-  context 'unsupported operating system' do
-    describe 'nsq class without any parameters on Solaris/Nexenta' do
-      let(:facts) do
-        {
-          :osfamily        => 'Solaris',
-          :operatingsystem => 'Nexenta',
-        }
-      end
-
-      it { expect { is_expected.to contain_package('nsq') }.to raise_error(Puppet::Error, /Nexenta not supported/) }
     end
   end
 end
